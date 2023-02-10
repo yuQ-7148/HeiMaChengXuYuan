d2c503bae02dfe9ade75c94371399429f9edd90a
yuQ-7148 <1159915745@qq.com>
2023-02-10 17:24:55 +0800

    输入方式改用Scanner

--- src/com/heima/javarumen/OperatorDemo2.java
+++ src/com/heima/javarumen/OperatorDemo2.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * @author yuQ-7148
@@ -13,10 +14,10 @@ public class OperatorDemo2 {
     public static void main(String[] args) throws IOException {
         //需求：一个三位数，将其拆分为个位、十位、百位，输出在控制台
         //输入数
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        int data = Integer.parseInt(str);
+        Scanner scan = new Scanner(System.in);
         System.out.println("请输入三位数");
+        String str = scan.nextLine();
+        int data = Integer.parseInt(str);
         System.out.println("输入三位数为:" + data);
         //个位
         System.out.println("个位数为:" + data % 10);

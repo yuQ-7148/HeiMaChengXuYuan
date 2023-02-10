617f1138635b03669b59992abec002fae0b025a4
yuQ-7148 <1159915745@qq.com>
2023-02-10 16:30:34 +0800

    一个三位数，将其拆分为个位、十位、百位，输出在控制台

--- src/com/heima/javarumen/OperatorTest2.java
+++ src/com/heima/javarumen/OperatorTest2.java
@@ -1,8 +1,28 @@
 package com.heima.javarumen;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author yuQ-7148
  * @date 2023/2/10
  **/
 public class OperatorTest2 {
+    public static void main(String[] args) throws IOException {
+        //需求：一个三位数，将其拆分为个位、十位、百位，输出在控制台
+        //输入数
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int data = Integer.parseInt(str);
+        System.out.println("请输入三位数");
+        System.out.println("输入三位数为:" + data);
+        //个位
+        System.out.println("个位数为:" + data % 10);
+        //十位
+        System.out.println("十位数为:" + (data / 10) % 10);
+        //百位
+        System.out.println("百位数为:" + data / 100);
+    }
 }
